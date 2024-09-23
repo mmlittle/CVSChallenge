d0d1726ed302d90565ced6ccd7ef256acde04660
Mike Little <mmlittle@yahoo.com>
2024-09-23 15:06:14 -0500

    Initial Commit

---
+++ CVSChallenge/CVSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSChallengeApp.swift
+//  CVSChallenge
+//
+//  Created by llamaman on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CVSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
